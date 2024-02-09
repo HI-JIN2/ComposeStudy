9aea141d7ac74e6c629c0710047db951d2309fcc
yujin <jini1514@soongsil.ac.kr>
2024-02-10 00:17:34 +0900

    add: Ch.20 작성 중

--- Compose20/app/src/main/java/com/compose/compose20/MainActivity.kt
+++ Compose20/app/src/main/java/com/compose/compose20/MainActivity.kt
@@ -3,11 +3,17 @@ package com.compose.compose20
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.Switch
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.compose.compose20.ui.theme.Compose20Theme
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DemoScreen()
                 }
             }
         }
@@ -30,17 +36,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun DemoScreen() {
+    MyTextField()
+}
+
+@Composable
+fun MyTextField() {
+    var textState = remember { mutableStateOf("") }
+
+    val onTextChange = { text: String ->
+        textState.value = text
+    }
+
+    TextField(value = textState.value, onValueChange = onTextChange)
+}
+
+@Composable
+fun FunctionA() {
+    var switchState by remember { mutableStateOf(true) }
+
+    val onSwitchChange = { value: Boolean ->
+        switchState = value
+    }
+
+    FunctionB(
+        switchState = switchState,
+        onSwitchChange = onSwitchChange
+    )
+}
+
+@Composable
+fun FunctionB(switchState: Boolean, onSwitchChange: (Boolean) -> Unit) {
+    Switch(
+        checked = switchState,
+        onCheckedChange = onSwitchChange
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     Compose20Theme {
-        Greeting("Android")
+        Column {
+            DemoScreen()
+            FunctionA()
+        }
     }
 }

ca2e1a52c7ea2caab0eb6f73fa9fad21d8f34e62
yujin <jini1514@soongsil.ac.kr>
2024-02-10 15:59:09 +0900

    feat: 컴포즈에서의 상태

--- CodeLabBasics/app/src/main/java/com/compose/basics/MainActivity.kt
+++ CodeLabBasics/app/src/main/java/com/compose/basics/MainActivity.kt
@@ -12,6 +12,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,17 +44,23 @@ fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("yu", "jin
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    var expanded by remember { mutableStateOf(false) }
+    var extarPadding = if (expanded) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extarPadding)) {
                 Text(text = "Hello")
                 Text(text = name)
             }
-            ElevatedButton(onClick = { /*TODO*/ }) {
-                Text("show more")
+            ElevatedButton(onClick = { expanded = !expanded }) {
+                Text(if (expanded) "show less" else "show more")
             }
         }
     }

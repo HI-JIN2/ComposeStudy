743caf67d9885d7c7b9e707fa2d37e6464f037a0
yujin <jini1514@soongsil.ac.kr>
2024-02-10 16:07:06 +0900

    feat: 상태 호이스팅 - 새 화면 추가

--- CodeLabBasics/app/src/main/java/com/compose/basics/MainActivity.kt
+++ CodeLabBasics/app/src/main/java/com/compose/basics/MainActivity.kt
@@ -3,10 +3,12 @@ package com.compose.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -16,6 +18,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,8 +30,27 @@ class MainActivity : ComponentActivity() {
         setContent {
             CodeLabBasicsTheme {
                 // A surface container using the 'background' color from the theme
-              MyApp(modifier = Modifier.fillMaxSize())
+                MyApp(modifier = Modifier.fillMaxSize())
             }
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("welcome to the basics codelab")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = { shouldShowOnboarding = false }
+        ) {
+            Text("continue")
         }
     }
 }
@@ -66,10 +88,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun GreetingPreview() {
     CodeLabBasicsTheme {
-        MyApp()
+//        MyApp()
+        OnboardingScreen()
     }
 }
